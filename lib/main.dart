c084010a052ec8cae19cb60f4b4d776dd74a1f11
Catalin Vunvulea <catalin.vunvulea@gmail.com>
2020-06-11 16:35:40 +0100

    Stateles Widget and Hot Reload
    create widget that will upate screen whenever you make a change, without requering to pressing any button

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text (
-            "My firs text on an app bar",
-        style: TextStyle(
-            fontSize: 20
-        ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.red[800], // press CtrQ when select colour and it will open full range
-      ),
-      body:Center(
-        child: Text (
-            "The text of my body",
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 1,
-          color: Colors.grey[500],
-          fontFamily: "IndieFLower"
-        ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text(
-            "+",
-        style: TextStyle(
-            fontSize: 35
-        ),
-        ),
-        backgroundColor: Colors.red[500],
-      ),
-    ) ,
-
-
+    home: Home()
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text (
+            "My firs text on an app bar",
+            style: TextStyle(
+                fontSize: 22
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red[800], // press CtrQ when select colour and it will open full range
+        ),
+        body:Center(
+          child: Text (
+            "The text of my body",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+                color: Colors.grey[500],
+                fontFamily: "IndieFLower"
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text(
+            "+",
+            style: TextStyle(
+                fontSize: 35
+            ),
+          ),
+          backgroundColor: Colors.red[500],
+        ),
+      );
+  }
+}
